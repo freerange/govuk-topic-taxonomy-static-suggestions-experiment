0cfd3d6ea9ea2393cd9e122b3d319640335b6657
Chris Lowis <chris.lowis@gofreerange.com>
2026-03-09 11:29:14 +0000

    Update query to return only whitehall documents with taxons
    We imagine our prototype being used to show how topic suggestion can
    work for whitehall publishers, so restricting the query to those
    documents makes sense. Docs without taxons are not useful for making
    suggestions.

--- query.sql
+++ query.sql
@@ -1,14 +1,13 @@
 COPY (
-  with content_items_cleaned as (
-    select
-      id,
-      base_path,
-      title,
-      details->'body' as body
-    from content_items
-  )
-  select *
-  from content_items_cleaned
+  select
+    id,
+    base_path,
+    title,
+    details->'body' as body,
+    expanded_links->'taxons' as taxons
+  from content_items
+  where publishing_app = 'whitehall'
+  and expanded_links?'taxons'
   order by id asc
   limit 50
 ) to STDOUT WITH CSV HEADER;
